2c388f83ad0aabd77f69f67588cfb21910ea999f
dave <dave@172-16-218-186.DYNAPOOL.NYU.EDU>
2016-11-22 17:20:04 -0500

    Put locations for cue cards along the four walls

--- Holojam/Assets/TapToChange.cs
+++ Holojam/Assets/TapToChange.cs
@@ -22,7 +22,7 @@ namespace Holojam.Tools
 		void Update ()
 		{
 			if (Input.GetMouseButtonDown (0)) {
-				if (activated) {
+				if (activated) {   /// console stays in front of camera (heads-up teleprompter)
 					activated = false;
 					VRConsole.parent = originpos.parent;
 					VRConsole.localPosition = originpos.localPosition;
@@ -32,16 +32,20 @@ namespace Holojam.Tools
 					activated = true;
 				}
 			}
-			if (activated) {
+			if (activated) {  // fix console in place in scene (aka "cue cards")
+				// tho actually, console will move to one of the fixed places in the scene where colliders
+				// tagged as "Player" are...
+
 				Vector3 fwd = viewer.actor.look;
 				//		Debug.Log (fw	d);
 				//Debug.DrawRay (cameratransform.position, fwd * 10000, Color.green);
+				// Look for one of the "Tele" colliders -- tagged as "Player" (should change this tag name)
 				RaycastHit hit;
 				if (Physics.Raycast (transform.position, fwd, out hit, 100000)) {
 					//Debug.Log (hit.transform.gameObject.name);
 					if (hit.transform.gameObject.tag == "Player") {
 					
-						VRConsole.parent = hit.transform;
+						VRConsole.parent = hit.transform;  // put console where the "Tele" collider is, then adjust:
 						VRConsole.localPosition = new Vector3 (-0.5f, 0f, 0f);
 						VRConsole.localRotation = Quaternion.identity;
 						VRConsole.localScale = new Vector3 (0.001765348f, 0.002229395f, 0.002229395f) * 2f;
